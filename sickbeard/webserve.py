c8fc35e4bf80c96946557de1d34ec7719ebd294a
Rob Bruce <rob.bruce70@gmail.com>
2010-08-24 00:07:00 -0600

    Fixed a typo

--- sickbeard/webserve.py
+++ sickbeard/webserve.py
@@ -643,7 +643,6 @@ class ConfigEpisodeDownloads:
         if root_dir and root_label: 
        		root_dirs = root_dirs + "|" + os.path.normpath(root_dir.strip())
         	root_labels = root_labels + "|" + root_label.strip()
-        end if
         
         if not config.change_TV_DOWNLOAD_DIR(tv_download_dir):
             results += ["Unable to create directory " + os.path.normpath(tv_download_dir) + ", dir not changed."]
